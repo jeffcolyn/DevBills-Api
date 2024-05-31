57f05f0d11e1df38bf2d8fff5afa2cc69d5b1632
jeffcolyn <jeferson.barbosa101@gmail.com>
2024-05-31 10:33:59 -0300

    apidevbills

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express, { json } from 'express';
+
+import { setupMongo } from './database';
+import { errorHandler } from './middlewares/error-handler.middleware';
+import { routes } from './routes';
+
+setupMongo().then(() => {
+  const app = express();
+
+  app.use(
+    cors({
+      origin: process.env.FRONT_URL,
+    }),
+  );
+  app.use(json());
+  app.use(routes);
+  app.use(errorHandler);
+
+  app.listen(3333, () => console.log('🚀 App is running at port 3333!'));
+});
